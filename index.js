5686c3356b583dc29774d9f9fcd06621ae0773c0
sharyar <sharyarmalik09@gmail.com>
2022-04-08 22:37:04 +0500

    Update index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const req = require('express/lib/request')
 const app = express()
 const port = 4000
 
@@ -7,9 +8,18 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  res.send('Hello About!')
+  res.send(' About!')
+})
+app.get('/abc', (req, res) => {
+  res.send(' abc!')
+})
+
+
+
+app.get('/contact', (req,res) =>{
+  res.send ('Contact!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
